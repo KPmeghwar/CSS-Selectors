08954704679c20de5b947d6561ce7f682f42388e
Kailash Meghwar <premanikailash97@gmail.com>
2022-03-08 21:27:24 +0500

    CSS-Selectors

---
+++ Used__selectors.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<m.cometa charset="utf-8">
+	<title>.::Used Selectors::.</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head*
+
+
+<body>
+        <h2>CSS Selectors</h2>
+        <hr><hr>
+        <h3>Element Selectors</h3>
+
+        <h1>CSS</h1>
+        <p>
+        Stands for "Cascading Style Sheet." Cascading style sheets are used to format the layout of Web pages. They can be used to define text styles, table sizes, and other aspects of Web pages that previously could only be defined in a pge's HTML.
+		CSS helps Web developers create a uniform look across several pages of a Web site. Instead of defining the style of each table and each block of text within a page's HTML, commonly used styles need to be defined only once in a CSS adocument. Once the style is defined in cascading style sheet, it can be used by any page that references the CSS file. Plus, CSS makes it easy to change styles across several pages at once. 
+	    </p>
+	    <h3>Class selectors</h3>
+	    <h1>PHP</h1>
+	    <p class="define">
+	   	Stands for "Hypertext Preprocessor." (It is a recursive acronym, if you can understand what that means.) PHP is an HTML-embedded Web scripting language. This means PHP code can be inserted into the HTML of a Web page. When a PHP page is accessed, the PHP code is read or "parsed" by the server the page resides on. The output from the PHP functions on the page are typically returned as HTML code, which can be read by the browser. Because the PHP code is transformed into HTML before the page is loaded, users cannot view the PHP code on a page. This make PHP pages secure enough to access databases and other secure information.
+
+        </p> 
+
+
+       <h3> ID selectors</h3>
+       <h1>HTML</h1>
+       <p id="word">
+       	
+       	Stands for "Hypertext Markup Language." HTML is the language used to create webpages. "Hypertext"                                                      refers to the hyperlinks that an HTML page may contain. "Markup language" refers to the way tags are used to                                            define the page layout and elements within the page.
+
+        Below is an example of HTML used to define a basic webpage with a title and a single paragraph of text.
+       </p>
+
+       <h3> Child Selector</h3>
+       <h1>Unordered List</h1>
+       <ul type="square">
+       	   <li>PHP</li>
+       	   <li>CSS</li>
+       	   <li>HTML</li>
+       	   <li>JAVA</li>
+       </ul>
+
+        <h3>Group Selector</h3>
+        <h1>JAVASCRIPT</h1>
+        
+        <h4>Defination of javascript</h4>
+        <pre>
+        JavaScript is a programming language commonly used in web development.
+        It was originally developed by Netscape as a means to add dynamic and 
+        interactive elements to websites. While JavaScript is influenced by Java, 
+        the syntax is more similar to C and is based on ECMAScript, 
+        a scripting language developed by Sun Microsystems.
+        </pre>   
+
+
+        <h3>Attribute Selectors</h3>
+        <div class="left">
+               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        </body>
+        </html>
